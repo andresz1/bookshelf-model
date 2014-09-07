48370939f6f6fa50d74807ccaa800d34986cd9e5
root <root@devm.(none)>
2014-09-07 16:53:50 -0430

    Fixing some spaces in index.js

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ module.exports = function(bookshelf, dir) {
   dir += dir.substr(-1) === '/' ? '' : '/';
 
   fs
-    .readdirSync(dir).filter(function (file) 
-    {
-      return ((file.indexOf('.') !== 0) && (file.slice(-3) == '.js'));
-    })
-    .forEach(function(file) 
-    {
-      var model = require(path.join(dir, file))(bookshelf); 
-      var name = file[0].toUpperCase() + file.slice(1, file.length - 3).replace(/_([a-z])/g, function (str) { 
-        return str[1].toUpperCase(); 
-      });
-      
-      models[name] = model;
+  .readdirSync(dir).filter(function (file) 
+  {
+    return ((file.indexOf('.') !== 0) && (file.slice(-3) == '.js'));
+  })
+  .forEach(function(file) 
+  {
+    var model = require(path.join(dir, file))(bookshelf); 
+    var name = file[0].toUpperCase() + file.slice(1, file.length - 3).replace(/_([a-z])/g, function (str) { 
+      return str[1].toUpperCase(); 
     });
+    
+    models[name] = model;
+  });
 
   Object.keys(models).forEach(function (key) 
   {
